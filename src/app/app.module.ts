605e454d5802cc38e14afa4d4ade1bb32091b99b
Petra Lignell <petralig@ety-a2123b-12.edu.wks.metropolia.fi>
2017-02-28 13:40:08 +0200

    update 28.2.2017 P

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {decimalDigest} from "@angular/compiler/src/i18n/digest";
+import {TodosComponent} from "./todos/todos.component";
+import {UserComponent} from "./user/user.component";
+import {ProgressbarComponent} from "./progressbar/progressbar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    UserComponent,
+    ProgressbarComponent,
   ],
   imports: [
     BrowserModule,
