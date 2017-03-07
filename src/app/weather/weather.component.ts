e9117c88a04c98d67a4c0d8d0a35100d0afda163
Petra Lignell <petralig@ety-a2123b-12.edu.wks.metropolia.fi>
2017-03-07 09:40:06 +0200

    update 7.3.2017 AP

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -9,25 +9,38 @@ import {Http, Response} from "@angular/http";
 export class WeatherComponent implements OnInit {
 
   private apinVastaus: any = {};
+  private paikka: any = {};
+  private lampotilat: any = {};
+  private kaupunki: string;
+  private maa: string;
+  private city: string;
+
 
   constructor(private http: Http) { }
 
   private getApi() {
-      this.http.get('http://api.aerisapi.com//observations/helsinki,fi?client_id=cIXZBPPK70aGNPlxYsWzB&client_secret=vuDLfmtvIavS3zmtU35PcTXtpfdmUTTr5KHO1G3S')
+      this.http.get('http://api.aerisapi.com//observations/'+this.kaupunki+','+this.maa+'?client_id=cIXZBPPK70aGNPlxYsWzB&client_secret=vuDLfmtvIavS3zmtU35PcTXtpfdmUTTr5KHO1G3S')
         .subscribe(
           (res:Response) => {
             this.apinVastaus = res.json();
-            console.log(this.apinVastaus);
+            //console.log(this.apinVastaus);
+            this.paikka = this.apinVastaus.response.place;
+            this.lampotilat = this.apinVastaus.response.ob;
+            console.log(this.paikka);
           }
         );
     }
 
+    setPlace = (city: string) => {
+      this.city = city;
+    };
+
+    getPlace = () => {
+      return this.city;
+    };
 
   ngOnInit() {
     this.getApi();
   }
 
 }
-
-
-
